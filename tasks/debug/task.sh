9757bfb8c892b8701cad9a8fb0184d318694fcaa
gmerlin <gmerlin@mg-bastion.mg.lab>
2018-04-11 17:20:53 -0700

    blah

--- tasks/debug/task.sh
+++ tasks/debug/task.sh
@@ -2,6 +2,17 @@
 set -e
 
 
-find / -name \* -print
+#find / -name \* -print
+ls -LR ~/gitroot/
+
+export build="git-go-http"
+
+export dockerfile="${build}/Dockerfile"
+
+
+  if [ ! -f "$dockerfile" ]; then
+    echo "It doesn't appear that given Dockerfile: \"$dockerfile\" is a file"
+    exit 1
+  fi
 
 exit 0
